c3dbc3617b73b5972ee690d4919b9c04861d54df
Ian <1984therocker@gmail.com>
2021-06-28 21:10:46 -0500

    Insert Index and Create

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+// import axios from "axios";
+
+// axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
 Vue.config.productionTip = false;
 
@@ -9,5 +12,4 @@ new Vue({
   render: (h) => h(App),
 }).$mount("#app");
 
-
-DREAMTEAM BABY
+// DREAMTEAM BABY
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,61 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="DREAM TEAM" />
+    <h1>New Student</h1>
+    <div>
+      Name:
+      <input type="text" v-model="newStudent.name" />
+      Resume:
+      <input type="text" v-model="newStudent.resume" />
+      Twitter:
+      <input type="text" v-model="newStudent.twitter" />
+      <button v-on:click="createStudent()">Create Student</button>
+    </div>
+
+    <h1>All Students</h1>
+    <div v-for="student in students" v-bind:key="student.id">
+      <h2>{{ student.name }}</h2>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<style></style>
 
+<script>
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      students: [],
+      newStudent: {},
+    };
+  },
+  created: function () {
+    this.indexStudents();
+  },
+  methods: {
+    indexStudents: function () {
+      axios.get("/students").then((response) => {
+        console.log("students index", response);
+        this.students = response.data;
+      });
+    },
+    createStudent: function () {
+      var params = {
+        name: this.newStudent.name,
+        resume: this.newStudent.resume,
+        twitter: this.newStudent.twitter,
+      };
+      axios
+        .post("/students", params)
+        .then((response) => {
+          console.log("students create", response);
+          this.students.push(response.data);
+          this.newStudent = {};
+        })
+        .catch((error) => {
+          console.log("students create error", error.response);
+        });
+    },
   },
 };
 </script>
