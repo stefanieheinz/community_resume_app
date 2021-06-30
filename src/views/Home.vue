0842d87e600bf863fb78f11ba8b2719ed2b3f6f8
Ryan Simms <simmsey53@gmail.com>
2021-06-29 21:55:49 -0400

    added more identifiers and corrected show more info method

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,22 @@
   <div class="home">
     <h1>All Students</h1>
     <div v-for="student in students" v-bind:key="student.id">
-      <h2>Name: {{ student.name }}</h2>
-      <h2>Resume: {{ student.resume }}</h2>
-      <h2>Twitter: {{ student.twitter }}</h2>
+      <p>First name: {{ student.first_name }}</p>
+      <p>Last name: {{ student.last_name }}</p>
+      <p>Email: {{ student.email }}</p>
       <button v-on:click="showStudent(student)">More info</button>
     </div>
     <dialog id="student-details">
       <form method="dialog">
         <h1>Student info</h1>
-        <p>Name: {{ currentStudent.name }}</p>
-        <p>Resume: {{ currentStudent.resume }}</p>
-        <p>Twitter: {{ currentStudent.twitter }}</p>
+        <p>Phone Number: {{ currentStudent.phone_number }}</p>
+        <p>Bio: {{ currentStudent.bio }}</p>
+        <p>LinkedIn: {{ currentStudent.linkedin }}</p>
+        <p>Twitter Handle: {{ currentStudent.twitter_handle }}</p>
+        <p>Website: {{ currentStudent.website }}</p>
+        <p>Resume URL: {{ currentStudent.resume_url }}</p>
+        <p>Github: {{ currentStudent.github }}</p>
+        <p>Photo: {{ currentStudent.photo }}</p>
         <button>Close</button>
       </form>
     </dialog>
@@ -27,11 +32,38 @@ export default {
   data: function () {
     return {
       students: [
-        { id: 1, name: "Jill", resume: "Worked on a crab boat", twitter: "twitter.com/jill35" },
-        { id: 2, name: "Jack", resume: "Worked on a rad boat", twitter: "twitter.com/jack32" },
-        { id: 3, name: "Ryan", resume: "Very handsome", twitter: "twitter.com/simmz53" },
+        {
+          id: 4,
+          first_name: "Bob",
+          last_name: "Ross",
+          email: "like_a_bross@tet.com",
+          password: "pasword",
+          phone_number: "1-800-BO-ROSS",
+          bio: "Just a happy little accident.",
+          linkedin: "linkedin.com/bbross",
+          twitter_handle: "like_abross",
+          website: "bobros.com",
+          resume_url: "drive.google.com/bobross-esume",
+          github: "github.com/bbross",
+          photo: "bros.jpg",
+        },
+        {
+          id: 5,
+          first_name: "Bill",
+          last_name: "Doss",
+          email: "like_a_bDoss@tet.com",
+          password: "pasword",
+          phone_number: "1-800-BO-Doss",
+          bio: "Just a happy little accident.",
+          linkedin: "linkedin.com/bbDoss",
+          twitter_handle: "like_abDoss",
+          website: "bobros.com",
+          resume_url: "drive.google.com/bobDoss-esume",
+          github: "github.com/bbDoss",
+          photo: "bros.jpg",
+        },
       ],
-      currentStudent: [{ id: 3, name: "Ryan", resume: "Very handsome", twitter: "twitter.com/simmz53" }],
+      currentStudent: {},
     };
   },
 
