a14bd10200edf0ec8b27743b09e2a950679ae0fd
Ryan Simms <simmsey53@gmail.com>
2021-06-29 21:29:16 -0400

    added hard coded data to test functionality

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,60 +1,53 @@
 <template>
   <div class="home">
-    <h1>New Student</h1>
-    <div>
-      Name:
-      <input type="text" v-model="newStudent.name" />
-      Resume:
-      <input type="text" v-model="newStudent.resume" />
-      Twitter:
-      <input type="text" v-model="newStudent.twitter" />
-      <button v-on:click="createStudent()">Create Student</button>
-    </div>
-
     <h1>All Students</h1>
     <div v-for="student in students" v-bind:key="student.id">
-      <h2>{{ student.name }}</h2>
+      <h2>Name: {{ student.name }}</h2>
+      <h2>Resume: {{ student.resume }}</h2>
+      <h2>Twitter: {{ student.twitter }}</h2>
+      <button v-on:click="showStudent(student)">More info</button>
     </div>
+    <dialog id="student-details">
+      <form method="dialog">
+        <h1>Student info</h1>
+        <p>Name: {{ currentStudent.name }}</p>
+        <p>Resume: {{ currentStudent.resume }}</p>
+        <p>Twitter: {{ currentStudent.twitter }}</p>
+        <button>Close</button>
+      </form>
+    </dialog>
   </div>
 </template>
 
 <style></style>
 
 <script>
-import axios from "axios";
+// import axios from "axios";
 export default {
   data: function () {
     return {
-      students: [],
-      newStudent: {},
+      students: [
+        { id: 1, name: "Jill", resume: "Worked on a crab boat", twitter: "twitter.com/jill35" },
+        { id: 2, name: "Jack", resume: "Worked on a rad boat", twitter: "twitter.com/jack32" },
+        { id: 3, name: "Ryan", resume: "Very handsome", twitter: "twitter.com/simmz53" },
+      ],
+      currentStudent: [{ id: 3, name: "Ryan", resume: "Very handsome", twitter: "twitter.com/simmz53" }],
     };
   },
-  created: function () {
-    this.indexStudents();
-  },
+
+  // created: function () {
+  //   this.indexStudents();
+  // },
   methods: {
-    indexStudents: function () {
-      axios.get("/students").then((response) => {
-        console.log("students index", response);
-        this.students = response.data;
-      });
-    },
-    createStudent: function () {
-      var params = {
-        name: this.newStudent.name,
-        resume: this.newStudent.resume,
-        twitter: this.newStudent.twitter,
-      };
-      axios
-        .post("/students", params)
-        .then((response) => {
-          console.log("students create", response);
-          this.students.push(response.data);
-          this.newStudent = {};
-        })
-        .catch((error) => {
-          console.log("students create error", error.response);
-        });
+    // indexStudents: function () {
+    //   axios.get("/students").then((response) => {
+    //     console.log("students index", response);
+    //     this.students = response.data;
+    //   });
+    // },
+    showStudent: function (student) {
+      this.currentStudent = student;
+      document.querySelector("#student-details").showModal();
     },
   },
 };
