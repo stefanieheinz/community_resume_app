5ee96ec3edf8dfad1f8a0de9a81dcd72f791e1d6
Ryan Simms <simmsey53@gmail.com>
2021-06-30 19:36:01 -0400

    changed from hard coded data to using backend database

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -27,56 +27,25 @@
 <style></style>
 
 <script>
-// import axios from "axios";
+import axios from "axios";
 export default {
   data: function () {
     return {
-      students: [
-        {
-          id: 4,
-          first_name: "Bob",
-          last_name: "Ross",
-          email: "like_a_bross@tet.com",
-          password: "pasword",
-          phone_number: "1-800-BO-ROSS",
-          bio: "Just a happy little accident.",
-          linkedin: "linkedin.com/bbross",
-          twitter_handle: "like_abross",
-          website: "bobros.com",
-          resume_url: "drive.google.com/bobross-esume",
-          github: "github.com/bbross",
-          photo: "bros.jpg",
-        },
-        {
-          id: 5,
-          first_name: "Bill",
-          last_name: "Doss",
-          email: "like_a_bDoss@tet.com",
-          password: "pasword",
-          phone_number: "1-800-BO-Doss",
-          bio: "Just a happy little accident.",
-          linkedin: "linkedin.com/bbDoss",
-          twitter_handle: "like_abDoss",
-          website: "bobros.com",
-          resume_url: "drive.google.com/bobDoss-esume",
-          github: "github.com/bbDoss",
-          photo: "bros.jpg",
-        },
-      ],
+      students: [],
       currentStudent: {},
     };
   },
 
-  // created: function () {
-  //   this.indexStudents();
-  // },
+  created: function () {
+    this.indexStudents();
+  },
   methods: {
-    // indexStudents: function () {
-    //   axios.get("/students").then((response) => {
-    //     console.log("students index", response);
-    //     this.students = response.data;
-    //   });
-    // },
+    indexStudents: function () {
+      axios.get("/students").then((response) => {
+        console.log("students index", response);
+        this.students = response.data;
+      });
+    },
     showStudent: function (student) {
       this.currentStudent = student;
       document.querySelector("#student-details").showModal();
